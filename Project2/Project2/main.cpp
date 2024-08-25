381afb7703c55b14bdd85b7361bb7c92442a0a94
PowerOleg <geraskin.oleg@mail.ru>
2024-08-25 19:11:11 +0300

    task2 is done

--- Project2/Project2/main.cpp
+++ Project2/Project2/main.cpp
@@ -1,64 +1,91 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
-#include <mutex>
+#include <string>
+#include <vector>
 
-void print(const size_t column1, const std::thread::id thread_id, const std::string column3, const std::string column4);
+void refresh_console(const size_t column1, const std::thread::id* thread_ids, const int* progress, const std::string* elapsed_time);
 
-double worker(const size_t thread_count, const int length)
+
+void worker(const size_t line_number, std::thread::id* thread_ids, int* progress_line, std::string* elapsed_time, const int iteration_quantity)
 {
-	char square = (char)254u;
+	double ratio = 20.0 / static_cast<double>(iteration_quantity);//a ratio to transform iteration_quantity into 20 units
+
 	const std::thread::id thread_id = std::this_thread::get_id();
-
-
-	std::string progress = "";//"                      ";//22 spaces
-	progress = square;
-	
-
+	thread_ids[line_number] = thread_id;
 
 	auto start = std::chrono::steady_clock::now();
-	for (size_t i = 0; i < length; i++)
+	size_t i = 0;
+	int result = 0;
+	while (result < iteration_quantity)//work simulating
 	{
-		print(thread_count, thread_id, progress, "");
+		result = ++i / (line_number + 1);
+		progress_line[line_number] = result * ratio;
+		std::this_thread::sleep_for(std::chrono::milliseconds(500));
 	}
 
 	auto end = std::chrono::steady_clock::now();
 	std::chrono::duration<long, std::nano> time = end - start;
 
-	double d_time = static_cast<double>(time.count()) / 1000000000;
-	return d_time;
+	double d_time = static_cast<double>(time.count()) / 1000000000;	
+	elapsed_time[line_number] = std::to_string(d_time);
 }
 
-void print(const size_t column1, const std::thread::id thread_id, const std::string column3, const std::string column4)
+void refresh_console(const size_t size, const std::thread::id* thread_ids, const int* progress, const std::string* elapsed_time)
 {
-	std::cout << column1 << "\t" << "\t" << thread_id << " " << column3 << "\t" << "\t" << column4 << "\t" << std::endl;
+	char square = (char)254u;
+	std::vector<std::string> progress_string(size, "");
+	for (size_t i = 0; i < size; i++)
+	{
+		for (size_t j = 0; j < progress[i]; j++)
+		{
+			progress_string[i] += square;
+		}
+	}
+
+
+	std::system("cls");
+	std::cout << "#" << "\t" << "\t" << "ID" << "\t" << "\t" << "Progress Bar" << "\t" << "\t" << "Elapsed Time" << "\t" << std::endl;
+	for (size_t i = 0; i < size; i++)
+	{
+		std::cout << i << "\t" << "\t" << thread_ids[i] << "\t" << progress_string[i] << "\t" << "\t" << elapsed_time[i] << "\t" << std::endl;
+	}
 }
 
 
 int main(int argc, char** argv)
 {
-	const int THREADS_QUANTITY = 2;
-	const int COMPUTING_LENGTH = 1;
+	const size_t THREADS_QUANTITY = 4;
+	const int ITERATION_QUANTITY = 10;
+	const size_t PROGRESS_BAR_LENGTH = 20;
 
-	std::cout << "#" << "\t" << "\t" << "id" << "\t" << "\t" << "Progress Bar" << "\t" << "\t" << "Elapsed time" << "\t" << std::endl;
+	std::thread::id* thread_ids = new std::thread::id[THREADS_QUANTITY];
+	int* progress_line = new int[THREADS_QUANTITY];
+	std::string* elapsed_time = new std::string[THREADS_QUANTITY];
+	for (size_t i = 0; i < THREADS_QUANTITY; i++)
+	{
+		progress_line[i] = 0;
+		elapsed_time[i] = "";
+	}
 
+	
 	std::thread threads[THREADS_QUANTITY];
 	for (size_t i = 0; i < THREADS_QUANTITY; i++)
 	{
-		threads[i] = std::thread(worker, i, COMPUTING_LENGTH);
-		threads[i].join();
+		threads[i] = std::thread(worker, i, std::ref(thread_ids), std::ref(progress_line), std::ref(elapsed_time), ITERATION_QUANTITY);
+		threads[i].detach();
 	}
-	
+	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
-
-
-
-
-
-
-
-
-
-
+	int count_end = 0;
+	while(elapsed_time[0] == "" || elapsed_time[1] == "" || elapsed_time[2] == "" || elapsed_time[3] == "")
+	{
+		refresh_console(THREADS_QUANTITY, thread_ids, progress_line, elapsed_time);
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+	}
+	refresh_console(THREADS_QUANTITY, thread_ids, progress_line, elapsed_time);
+	delete[] thread_ids;
+	delete[] progress_line;
+	delete[] elapsed_time;
 	return 0;
 }
