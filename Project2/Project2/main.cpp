20df8765342e3d42e36506d6da45da315d770152
PowerOleg <geraskin.oleg@mail.ru>
2024-08-31 13:56:27 +0300

    task2;remaster1.2

--- Project2/Project2/main.cpp
+++ Project2/Project2/main.cpp
@@ -6,47 +6,23 @@
 #include <mutex>
 
 std::mutex m1;
-const size_t THREADS_QUANTITY = 4;
+const size_t THREADS_QUANTITY = 1;
 const int ITERATION_QUANTITY = 10;
+char SQUARE = (char)254u;
 
-void refresh_console(const size_t column1, const std::thread::id* thread_ids, const int* progress, const std::string* elapsed_time);
+//void refresh_console(const size_t column1, const std::thread::id* thread_ids, const int* progress, const std::string* elapsed_time);
 
-
-void draw(const size_t line_number, std::thread::id* thread_ids, int* progress_line, std::string* elapsed_time, const int iteration_quantity)
+void move_cursor_in_console(const int x, const int y)
 {
-	/*double ratio = 20.0 / static_cast<double>(iteration_quantity);//a ratio to transform iteration_quantity into 20 units
-
-	const std::thread::id thread_id = std::this_thread::get_id();
-	thread_ids[line_number] = thread_id;*/
-
-	auto start = std::chrono::steady_clock::now();
-
-
-	size_t i = 0;
-	int result = 0;
-	for (size_t i = 0; i < iteration_quantity; i++)//work simulating
-	{
-		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-		std::unique_lock<std::mutex> lk1(m1);
-
-		refresh_console(THREADS_QUANTITY, thread_ids, progress_line, elapsed_time);
-		
-
-
-
-
-
-		lk1.unlock();
-	}
-
-	auto end = std::chrono::steady_clock::now();
-	std::chrono::duration<long, std::nano> time = end - start;
-
-	double d_time = static_cast<double>(time.count()) / 1000000000;	
-	elapsed_time[line_number] = std::to_string(d_time);
+	printf("\033[%d;%dH", y, x);
 }
 
-void refresh_console(const size_t size, const std::thread::id* thread_ids, const int* progress, const std::string* elapsed_time)
+void print_square()
+{
+	std::cout << SQUARE;
+}
+
+void print_title(const size_t size, const std::thread::id* thread_ids/*, const int* progress, const std::string* elapsed_time*/, const size_t line_number)
 {
 	char square = (char)254u;
 	std::vector<std::string> progress_string(size, "");
@@ -67,10 +43,40 @@ void refresh_console(const size_t size, const std::thread::id* thread_ids, const
 	}
 }
 
+void draw(const size_t line_number, std::thread::id* thread_ids, int* progress_line, std::string* elapsed_time, const int iteration_quantity)
+{
+	/*double ratio = 20.0 / static_cast<double>(iteration_quantity);//a ratio to transform iteration_quantity into 20 units
+
+	const std::thread::id thread_id = std::this_thread::get_id();
+	thread_ids[line_number] = thread_id;*/
+	print_title(THREADS_QUANTITY, thread_ids, line_number);
+	auto start = std::chrono::steady_clock::now();
+
+
+	size_t i = 0;
+	int result = 0;
+
+	for (size_t i = 0; i < iteration_quantity; i++)//work simulating
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000 / line_number));
+		std::unique_lock<std::mutex> lk1(m1);
+
+		move_cursor_in_console(1, 10);
+		print_square();
+
+
+		lk1.unlock();
+	}
+
+	auto end = std::chrono::steady_clock::now();
+	std::chrono::duration<long, std::nano> time = end - start;
+	double d_time = static_cast<double>(time.count()) / 1000000000;
+	elapsed_time[line_number] = std::to_string(d_time);
+	//ка коплучили время надо дописать в консоль elapsed_time
+}
 
 int main(int argc, char** argv)
 {
-
 	std::thread::id* thread_ids = new std::thread::id[THREADS_QUANTITY];
 	int* progress_line = new int[THREADS_QUANTITY];
 	std::string* elapsed_time = new std::string[THREADS_QUANTITY];
