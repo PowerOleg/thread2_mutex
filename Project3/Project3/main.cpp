ebe7ca65fe45c807245564c5b80fe6f0f9aa0af6
PowerOleg <geraskin.oleg@mail.ru>
2024-09-01 19:13:58 +0300

    task3 is done;swap

--- Project3/Project3/main.cpp
+++ Project3/Project3/main.cpp
@@ -4,10 +4,6 @@
 #include <thread>
 #include <functional>
 
-#include <chrono>
-#include <functional>
-#include <vector>
-
 void swap_lock_guard(Data& d1, Data& d2)
 {
 	std::lock_guard<std::mutex> lk1(d1.m1);
@@ -20,12 +16,27 @@ void swap_lock_guard(Data& d1, Data& d2)
 
 void swap_unique_lock(Data& d1, Data& d2)
 {
-	//std::lock_guard<std::mutex> lk1(d1.m1);
-	//std::lock_guard<std::mutex> lk2(d2.m1);
+	std::unique_lock<std::mutex> lk1(d1.m1);
+	std::unique_lock<std::mutex> lk2(d2.m1);
 
 	Data temp = std::move(d1);
 	d1 = std::move(d2);
 	d2 = std::move(temp);
+
+	lk1.unlock();
+	lk2.unlock();
+}
+
+void swap_lock(Data& d1, Data& d2)
+{
+	std::lock(d1.m1, d2.m1);
+
+	Data temp = std::move(d1);
+	d1 = std::move(d2);
+	d2 = std::move(temp);
+
+	d1.m1.unlock();
+	d2.m1.unlock();
 }
 
 std::ostream& operator<<(std::ostream& os, Data& data)
@@ -44,13 +55,14 @@ int main(int argc, char** argv)
 	std::cout << "Data1:" << d1 << std::endl;
 	std::cout << "Data2:" << d2 << std::endl << std::endl;
 	std::cout << &d1 << std::endl;
-	/*
+	
 	swap_unique_lock(d1, d2);
 	std::cout << "Data1:" << d1 << std::endl;
 	std::cout << "Data2:" << d2 << std::endl << std::endl;
+
 	swap_lock(d1, d2);
 	std::cout << "Data1:" << d1 << std::endl;
-	std::cout << "Data2:" << d2 << std::endl << std::endl;*/
+	std::cout << "Data2:" << d2 << std::endl << std::endl;
 
 	return 0;
 }
